91c37e1cb5fdb9a1b43334e31edf0e65461e1a1c
Shawn0604 <shawn901011@gmail.com>
2023-09-13 20:09:59 +0800

    renew(0913)
    renew(0913)

--- src/hooks/dashboard/useSkills.js
+++ src/hooks/dashboard/useSkills.js
@@ -2,8 +2,7 @@ import useSWRFetch from "../useSWRFetch";
 
 const useSkills = (studentId) => {
   try {
-    const { data } = useSWRFetch(
-    `https://api.projectszero.tech/skills/${studentId}` );
+    const { data } = useSWRFetch(`https://api.projectszero.tech/skills/${studentId}`);
 
     return {
       labels: [
@@ -14,12 +13,11 @@ const useSkills = (studentId) => {
         "frontend"
       ],
       
-      values: data?Object.values(data):[0,0,0,0,0]
+      values:data?Object.values(data):[0,0,0,0,0]
     };
   } catch (error) {
     console.error("發送錯誤:", error);
     throw error;
   };
 };
-
 export default useSkills;
